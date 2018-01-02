ccfcceb7ce56897937468cb86689a60fa1175488
Blender2016 <devahmedazzab2014@gmail.com>
2018-01-02 06:57:28 +0000

    fetch todo from db through app server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
 const {User} = require("./models/user");
@@ -30,6 +31,22 @@ app.get('/todos',(req,res)=>{
          console.log(err);
          res.status(400).send(err);
      });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        console.log("ahmed",todo);
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err)=>{
+       res.status(400).send();
+    });
 });
 
 app.listen(3000,()=>{
