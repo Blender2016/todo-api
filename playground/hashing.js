cf41ca178c6101a3d807318b2fbb9b3b6db60242
Blender2016 <devahmedazzab2014@gmail.com>
2018-01-03 10:56:38 +0000

    add POST /users route and taken method

---
+++ playground/hashing.js
@@ -0,0 +1,8 @@
+const {SHA256} =require('crypto-js'); 
+// import hashing function from crypto-js module.
+
+const jwt = require("jsonwebtoken");
+
+var message ="linux system administration"; 
+var hash = SHA256(message).toString();
+console.log(hash);
--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,53 @@
 const mongoose = require("mongoose");
-var User = mongoose.model('User',{
-    name:{
-        type:String,
-        trim:true,
-        required:true,
-        minlength:true
-    },
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _=require("lodash");
+const UserSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
         trim:true,
-        minlength:1
-    }
+        minlength:1,
+        unique:true,
+        validate:{
+            validator:validator.isEmail,
+            message:"{value} is not a valid email ."
+        }
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength:6,
+    },
+    tokens:[{
+        access:{
+            type:String,
+            require:true
+        },
+        token:{
+            type:String,
+            require:true
+        }
+    }]
 });
 
+ UserSchema.methods.toJSON=function(){
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject,['_id','email']);
+}
+
+ UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+  
+    user.tokens.push({access, token});
+  
+    return user.save().then(() => {
+      return token;
+    });
+  };
+
+var User = mongoose.model('User', UserSchema);
 module.exports={User};
